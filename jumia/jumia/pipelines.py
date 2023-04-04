32a8488834e575175a72e77338e317893bde2643
kimenyu <njorogekimenyu@gmail.com>
2023-04-04 17:02:30 +0300

    not complete

--- jumia/jumia/pipelines.py
+++ jumia/jumia/pipelines.py
@@ -16,8 +16,8 @@ class JumiaPipeline(object):
     def create_connection(self):
         self.conn = mysql.connector.connect(
             host = 'localhost',
-            user = 'root',
-            password = 'Boyfaded7552',
+            user = '**********',
+            password = '*****',
             database = 'jumia'
         )
         self.curr = self.conn.cursor()
